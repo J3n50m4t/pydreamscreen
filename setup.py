88350fadbe678b3d1380b0ae99161f5cddc95a83
GregoryDosh <GregoryDosh@users.noreply.github.com>
2018-02-12 12:06:53 -0600

    initial commit pt2

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyDreamScreen',
+    version='0.0.1',
+    packages=find_packages(),
+    author='Gregory Dosh',
+    description='Python device discovery and manipulation for DreamScreen HD, '
+                '4K, and SideKick devices.',
+    license='MIT',
+    url='https://github.com/GregoryDosh/pydreamscreen',
+    long_description=open('README.md').read(),
+    include_package_data=True,
+    install_requires=list(r.strip() for r in open('requirements.txt')),
+    platforms='any',
+    zip_safe=False,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
