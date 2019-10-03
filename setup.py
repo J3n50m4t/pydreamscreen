97062af9da78ba4b542c9a944b4a3752d3936caf
Markus Dittrich <markusdittrich1@gmail.com>
2019-10-03 19:47:15 +0200

    updating version number
    always forget this

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PyDreamScreen",
-    version="0.0.12",
+    version="0.0.13",
     packages=find_packages(),
     author="Gregory Dosh",
     author_email="pypi@gregorydosh.com",
